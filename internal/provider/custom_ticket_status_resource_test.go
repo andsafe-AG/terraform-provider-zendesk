158d4ba0ec05cbf6e49d12fadfb75b958b62c265
marat-aukhadiev <marat.aukhadiev@andsafe.de>
2024-06-27 18:16:29 +0200

    add mock server

--- internal/provider/custom_ticket_status_resource_test.go
+++ internal/provider/custom_ticket_status_resource_test.go
@@ -11,17 +11,40 @@
 package provider
 
 import (
+	"context"
 	"fmt"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"log"
+	"os"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/testcontainers/testcontainers-go"
 )
 
 func TestAccCustomStatusResource(t *testing.T) {
+
 	resource.Test(t, resource.TestCase{
-		//PreCheck:                 func() { testAccPreCheck(t) },
+
+		PreCheck: func() {
+			mockContainer, err := startMockServer()
+			if err != nil {
+				log.Default().Printf("Failed to start container %s\n", err)
+				t.Fatal(err)
+
+			}
+			t.Cleanup(func() {
+				log.Default().Printf("Stopping container %s\n", mockContainer.URI)
+				if err := mockContainer.Terminate(context.Background()); err != nil {
+					t.Fatalf("failed to terminate container: %s", err)
+				}
+			})
+		},
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
+
 			// Create and Read testing
 			{
 				Config: providerConfig + testAccCustomStatusResourceConfig("one"),
@@ -54,6 +77,70 @@ func TestAccCustomStatusResource(t *testing.T) {
 			// Delete testing automatically occurs in TestCase
 		},
 	})
+}
+
+type zendeskAPIMockContainer struct {
+	testcontainers.Container
+	URI string
+}
+
+func startMockServer() (*zendeskAPIMockContainer, error) {
+	ctx := context.Background()
+	dl := log.Default()
+	mockContainerName := "zendesk-mock"
+
+	path, err := getDockerfilePath()
+	if err != nil {
+		return nil, err
+	}
+
+	req := testcontainers.ContainerRequest{
+		FromDockerfile: testcontainers.FromDockerfile{
+			Context:       path,
+			Dockerfile:    "Dockerfile",
+			PrintBuildLog: true,
+		},
+		Name:         mockContainerName,
+		ExposedPorts: []string{"8080:8080/tcp"},
+		LifecycleHooks: []testcontainers.ContainerLifecycleHooks{
+			testcontainers.DefaultLoggingHook(dl),
+		},
+		WaitingFor: wait.ForLog("Mock engine up and running").WithStartupTimeout(90 * time.Second), // Container is ready
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	ip, err := container.Host(ctx)
+	if err != nil {
+		return nil, err
+	}
+	log.Default().Printf(" Host is: %v \n", ip)
+
+	mappedPort, err := container.MappedPort(ctx, "8080")
+	if err != nil {
+		return nil, err
+	}
+
+	uri := fmt.Sprintf("http://%s:%s", ip, mappedPort.Port())
+
+	return &zendeskAPIMockContainer{Container: container, URI: uri}, nil
+}
+
+func getDockerfilePath() (string, error) {
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	const ProviderPath = "internal/provider"
+	const MockDockerfilePath = "zendesk_api/mock"
+	path = strings.Replace(path, ProviderPath, MockDockerfilePath, 1)
+	return path, err
 }
 
 func testAccCustomStatusResourceConfig(agentLabel string) string {
