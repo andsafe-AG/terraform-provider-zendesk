5879ef3785e0f87bb4b294e03f80fbc6c39e1d4e
marat-aukhadiev <marat.aukhadiev@andsafe.de>
2024-07-26 07:56:22 +0200

    add signing secret

--- internal/provider/webhook_resource.go
+++ internal/provider/webhook_resource.go
@@ -65,7 +65,8 @@ func (r *webhookResource) ImportState(ctx context.Context, request resource.Impo
 	webhookIdPath2 := path.Root("webhook").AtName("id")
 	var webhookIdOfMatching string
 
-	webhookShowResponse, err := r.client.GetClient().ShowWebhookWithResponse(ctx, id, nil)
+	reqEditors := make([]zendesk_webhook_api.RequestEditorFn, 0)
+	webhookShowResponse, err := r.client.GetClient().ShowWebhookWithResponse(ctx, id, reqEditors...)
 	if err != nil {
 		tflog.Error(ctx, "Error reading webhook data from the API: ", map[string]interface{}{"error": err})
 		response.Diagnostics.AddError("Error reading webhook data from the API", err.Error())
@@ -74,7 +75,8 @@ func (r *webhookResource) ImportState(ctx context.Context, request resource.Impo
 
 	if webhookShowResponse.StatusCode() == 404 {
 
-		webhooks, err := r.client.GetClient().ListWebhooksWithResponse(ctx, nil, nil)
+		reqEditors := make([]zendesk_webhook_api.RequestEditorFn, 0)
+		webhooks, err := r.client.GetClient().ListWebhooksWithResponse(ctx, nil, reqEditors...)
 		if err != nil {
 			tflog.Error(ctx, "Error reading webhooks list from the API: ", map[string]interface{}{"error": err})
 			response.Diagnostics.AddError("Error reading webhooks list from the API", err.Error())
@@ -82,8 +84,8 @@ func (r *webhookResource) ImportState(ctx context.Context, request resource.Impo
 		}
 
 		if webhooks.StatusCode() != 200 {
-			tflog.Error(ctx, "Error reading webhooks list from the API: ", map[string]interface{}{"error": webhooks.Body})
-			response.Diagnostics.AddError("Error reading webhooks list from the API", fmt.Sprintf("%+v", webhooks.Body))
+			tflog.Error(ctx, "Error reading webhooks list from the API: ", map[string]interface{}{"error": string(webhooks.Body)})
+			response.Diagnostics.AddError("Error reading webhooks list from the API", fmt.Sprintf("%+v", string(webhooks.Body)))
 			return
 		}
 
@@ -97,8 +99,8 @@ func (r *webhookResource) ImportState(ctx context.Context, request resource.Impo
 		}
 
 	} else if webhookShowResponse.StatusCode() != 200 {
-		tflog.Error(ctx, "Error reading webhook data from the API: ", map[string]interface{}{"error": webhookShowResponse.Body})
-		response.Diagnostics.AddError("Error reading webhook data from the API", fmt.Sprintf("%+v", webhookShowResponse.Body))
+		tflog.Error(ctx, "Error reading webhook data from the API: ", map[string]interface{}{"error": string(webhookShowResponse.Body)})
+		response.Diagnostics.AddError("Error reading webhook data from the API", fmt.Sprintf("%+v", string(webhookShowResponse.Body)))
 		return
 	} else {
 		webhookIdOfMatching = *webhookShowResponse.JSON200.Webhook.Id
@@ -127,28 +129,66 @@ func (r *webhookResource) Create(ctx context.Context, req resource.CreateRequest
 		return
 	}
 
-	tflog.Debug(ctx, "Create webhook resource with id: "+fmt.Sprintf("%+v", planModel.WebhookId))
+	tflog.Debug(ctx, "Create webhook resource with model: "+fmt.Sprintf("%+v", &planModel))
+
 	webhookMapper := resource_webhook.NewWebhookMapper()
 	requestBody, diags := webhookMapper.MapToCreateRequestBody(ctx, &planModel)
 	if diags != nil && diags.HasError() {
 		resp.Diagnostics.Append(diags...)
 		return
 	}
-
-	createResponse, err := r.client.GetClient().CreateOrCloneWebhookWithResponse(ctx, nil, *requestBody, nil)
+	tflog.Debug(ctx, "Create webhook resource with request body: "+fmt.Sprintf("%+v", requestBody))
+	reqEditors := make([]zendesk_webhook_api.RequestEditorFn, 0)
+	createResponse, err := r.client.GetClient().CreateOrCloneWebhookWithResponse(ctx, nil, *requestBody, reqEditors...)
 	if err != nil {
 		tflog.Error(ctx, "Error creating webhook data from the API: ", map[string]interface{}{"error": err})
 		resp.Diagnostics.AddError("Error creating webhook data from the API", err.Error())
 		return
 	}
+	tflog.Debug(ctx, fmt.Sprintf("Received API response: Status %v, Body: %+v", createResponse.StatusCode(), string(createResponse.Body)))
+	if createResponse.StatusCode() != 201 {
+		tflog.Error(ctx, "Error creating webhook data from the API: ", map[string]interface{}{"error": string(createResponse.Body)})
+		body, _ := json.Marshal(requestBody)
+		resp.Diagnostics.AddError("Error creating webhook data from the API", fmt.Sprintf("Code: %v Body: %+v Request: %v", createResponse.StatusCode(), string(createResponse.Body), string(body)))
+		return
+	}
+	if createResponse.JSON201.Webhook.SigningSecret == nil {
+		createResponse.JSON201.Webhook.SigningSecret = &struct {
+			Algorithm *string `json:"algorithm,omitempty"`
+			Secret    *string `json:"secret,omitempty"`
+		}{
+			Algorithm: nil,
+			Secret:    nil,
+		}
+	}
+	signingSecret := createResponse.JSON201.Webhook.SigningSecret
 
-	diags2 := webhookMapper.PutCreateResponseToStateModel(ctx, createResponse, &planModel)
+	id := *createResponse.JSON201.Webhook.Id
+	fetchSigningSecret(ctx, r.client, id, signingSecret)
+
+	diags2 := webhookMapper.UpdateAttributesWithCreateResponse(ctx, createResponse, &planModel)
 	if diags2 != nil && diags2.HasError() {
+		tflog.Error(ctx, "Error updating webhook model with create response: ", map[string]any{"error": diags2.Errors()})
 		resp.Diagnostics.Append(diags2...)
 		return
 	}
+
 	// Save data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &planModel)...)
+}
+
+func fetchSigningSecret(ctx context.Context, client *zendesk_webhook_api.WebhookApi, id string, signingSecret *struct {
+	Algorithm *string `json:"algorithm,omitempty"`
+	Secret    *string `json:"secret,omitempty"`
+}) {
+	reqEditors := make([]zendesk_webhook_api.RequestEditorFn, 0)
+	responseSecret, errSecret := client.GetClient().ShowWebhookSigningSecretWithResponse(ctx, id, reqEditors...)
+	if errSecret != nil {
+		tflog.Error(ctx, "Error reading webhook signing secret from the API: ", map[string]interface{}{"error": errSecret})
+	} else if responseSecret != nil && responseSecret.StatusCode() == 200 {
+		signingSecret.Secret = responseSecret.JSON200.SigningSecret.Secret
+		signingSecret.Algorithm = responseSecret.JSON200.SigningSecret.Algorithm
+	}
 }
 
 func (r *webhookResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
@@ -163,10 +203,11 @@ func (r *webhookResource) Read(ctx context.Context, req resource.ReadRequest, re
 	}
 
 	tflog.Debug(ctx, "Read webhook resource with id: "+fmt.Sprintf("%+v", state.WebhookId))
-	webhookId := state.WebhookId.String()
+	webhookId := state.WebhookId.ValueString()
 
 	// Get Webhook data from the API
-	webhookResponse, err := r.client.GetClient().ShowWebhookWithResponse(ctx, webhookId, nil)
+	reqEditors := make([]zendesk_webhook_api.RequestEditorFn, 0)
+	webhookResponse, err := r.client.GetClient().ShowWebhookWithResponse(ctx, webhookId, reqEditors...)
 
 	if err != nil {
 		tflog.Error(ctx, "Error reading webhook data from the API: ", map[string]interface{}{"error": err})
@@ -175,11 +216,23 @@ func (r *webhookResource) Read(ctx context.Context, req resource.ReadRequest, re
 	}
 
 	if webhookResponse.StatusCode() != 200 {
-		tflog.Error(ctx, "Error reading webhook data from the API: ", map[string]interface{}{"error": webhookResponse.Body})
-		resp.Diagnostics.AddError("Error reading webhook data from the API", fmt.Sprintf("%+v", webhookResponse.Body))
+		message := fmt.Sprintf("Error reading webhook data from the API, StatusCode: %v , Request Param: %v", webhookResponse.StatusCode(), webhookId)
+		tflog.Error(ctx, message,
+			map[string]interface{}{"error": string(webhookResponse.Body)})
+		resp.Diagnostics.AddError("Error reading webhook data from the API", fmt.Sprintf("%+v", string(webhookResponse.Body)))
 		return
 	}
 
+	if webhookResponse.JSON200.Webhook.SigningSecret == nil {
+		webhookResponse.JSON200.Webhook.SigningSecret = &struct {
+			Algorithm *string `json:"algorithm,omitempty"`
+			Secret    *string `json:"secret,omitempty"`
+		}{
+			Algorithm: nil,
+			Secret:    nil,
+		}
+	}
+	fetchSigningSecret(ctx, r.client, webhookId, webhookResponse.JSON200.Webhook.SigningSecret)
 	webhookMapper := resource_webhook.NewWebhookMapper()
 
 	webhookMapper.PutWebhookShowResponseToStateModel(ctx, webhookResponse, &state)
@@ -193,10 +246,23 @@ func logErrors(ctx context.Context, resp *resource.ReadResponse) {
 }
 
 func (r *webhookResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var data resource_webhook.WebhookModel
+	var planModel resource_webhook.WebhookModel
 
+	tflog.Debug(ctx, "Update webhook resource")
 	// Read Terraform plan data into the model
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &planModel)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("Update webhook resource with model: %+v", &planModel))
+
+	// Read Terraform state data into the model
+	var stateModel resource_webhook.WebhookModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &stateModel)...)
+
+	tflog.Debug(ctx, fmt.Sprintf("Update webhook resource with previous state: %+v", &stateModel))
 
 	if resp.Diagnostics.HasError() {
 		return
@@ -204,13 +270,17 @@ func (r *webhookResource) Update(ctx context.Context, req resource.UpdateRequest
 
 	webhookMapper := resource_webhook.NewWebhookMapper()
 
-	mappedRequestBody, diags := webhookMapper.MapToUpdateRequestBody(ctx, &data)
+	mappedRequestBody, diags := webhookMapper.MapToUpdateRequestBody(ctx, &planModel)
+
 	if diags != nil && diags.HasError() {
 		resp.Diagnostics.Append(diags...)
 		return
 	}
+	tflog.Debug(ctx, fmt.Sprintf("Update webhook resource with mapped request body: %+v", &mappedRequestBody))
 
-	response, err := r.client.GetClient().UpdateWebhookWithResponse(ctx, data.WebhookId.ValueString(), mappedRequestBody, nil)
+	reqEditors := make([]zendesk_webhook_api.RequestEditorFn, 0)
+	tflog.Debug(ctx, fmt.Sprintf("Will update webhook with id %v with request body: %+v and reqEditors %v", stateModel.WebhookId.ValueString(), &mappedRequestBody, &reqEditors))
+	response, err := r.client.GetClient().UpdateWebhookWithResponse(ctx, stateModel.WebhookId.ValueString(), mappedRequestBody, reqEditors...)
 
 	if err != nil {
 		resp.Diagnostics.AddError("Error updating webhook data from the API", err.Error())
@@ -218,8 +288,10 @@ func (r *webhookResource) Update(ctx context.Context, req resource.UpdateRequest
 	}
 
 	if response.StatusCode() == 404 {
-		resp.Diagnostics.AddError("Error updating webhook data from the API", "Webhook not found")
+
+		resp.Diagnostics.AddError("Error updating webhook data", "Webhook not found: "+string(response.Body))
 		return
+
 	}
 
 	if response.StatusCode() == 400 {
@@ -233,22 +305,44 @@ func (r *webhookResource) Update(ctx context.Context, req resource.UpdateRequest
 
 	}
 	if response.StatusCode() != 204 {
-		detail := "Unexpected response status code: " + strconv.Itoa(response.StatusCode()) + ", Response Body: " + fmt.Sprintf("%+v", response.Body)
+		detail := "Unexpected response status code: " + strconv.Itoa(response.StatusCode()) + ", Response Body: " + fmt.Sprintf("%+v", string(response.Body))
 		resp.Diagnostics.AddError("Error updating webhook data from the API", detail)
 		return
 	}
-
-	showResponse, err := r.client.GetClient().ShowWebhookWithResponse(ctx, data.WebhookId.ValueString(), nil)
+	reqEditors2 := make([]zendesk_webhook_api.RequestEditorFn, 0)
+	tflog.Debug(ctx, fmt.Sprintf("Will get webhook with id %v with reqEditors %v", stateModel.WebhookId.ValueString(), reqEditors2))
+	showResponse, err := r.client.GetClient().ShowWebhookWithResponse(ctx, stateModel.WebhookId.ValueString(), reqEditors2...)
 
 	if err != nil {
 		resp.Diagnostics.AddError("Error reading webhook data from the API after successful update", err.Error())
 		return
 	}
 
-	webhookMapper.PutWebhookShowResponseToStateModel(ctx, showResponse, &data)
+	if showResponse.StatusCode() != 200 {
+		resp.Diagnostics.AddError(fmt.Sprintf("Error reading webhook data from the API after successful update with status %v", showResponse.StatusCode()), fmt.Sprintf("%+v", string(showResponse.Body)))
+		return
+	}
+
+	if showResponse.JSON200.Webhook.SigningSecret == nil {
+		showResponse.JSON200.Webhook.SigningSecret = &struct {
+			Algorithm *string `json:"algorithm,omitempty"`
+			Secret    *string `json:"secret,omitempty"`
+		}{
+			Algorithm: nil,
+			Secret:    nil,
+		}
+	}
+	fetchSigningSecret(ctx, r.client, stateModel.WebhookId.ValueString(), showResponse.JSON200.Webhook.SigningSecret)
+
+	diagnostics := webhookMapper.PutWebhookShowResponseAfterUpdateToStateModel(ctx, showResponse, &planModel)
+	if diagnostics != nil && diagnostics.HasError() {
+		tflog.Error(ctx, "Error updating webhook model with show response after successful update: ", map[string]any{"error": diagnostics.Errors()})
+		resp.Diagnostics.Append(diagnostics...)
+		return
+	}
 
 	// Save updated data into Terraform state
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &planModel)...)
 }
 
 func (r *webhookResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
@@ -261,7 +355,8 @@ func (r *webhookResource) Delete(ctx context.Context, req resource.DeleteRequest
 		return
 	}
 
-	response, err := r.client.GetClient().DeleteWebhookWithResponse(ctx, data.WebhookId.ValueString(), nil)
+	reqEditors := make([]zendesk_webhook_api.RequestEditorFn, 0)
+	response, err := r.client.GetClient().DeleteWebhookWithResponse(ctx, data.WebhookId.ValueString(), reqEditors...)
 	if err != nil {
 		resp.Diagnostics.AddError("Error deleting webhook data from the API", err.Error())
 		return
@@ -283,7 +378,7 @@ func (r *webhookResource) Delete(ctx context.Context, req resource.DeleteRequest
 	}
 
 	if response.StatusCode() != 204 {
-		detail := "Unexpected response status code: " + strconv.Itoa(response.StatusCode()) + ", Response Body: " + fmt.Sprintf("%+v", response.Body)
+		detail := "Unexpected response status code: " + strconv.Itoa(response.StatusCode()) + ", Response Body: " + fmt.Sprintf("%+v", string(response.Body))
 		resp.Diagnostics.AddError("Error deleting webhook data from the API", detail)
 		return
 	}
